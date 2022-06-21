6b7309f7942e4b2492fe9d6f849e6b00d5de6c42
Mr-Milk <zym.zym1220@gmail.com>
2022-06-21 20:38:51 +0800

    add preset

--- legendkit/_preset.py
+++ legendkit/_preset.py
@@ -8,7 +8,7 @@ class CatLegend(ListLegend):
     _sizer = {
         "small": 0.4,
         "medium": 0.7,
-        "large": 1.0,
+        "large": 1.1,
     }
 
     def __init__(self, colors, labels, size="small", handle=None, **kwargs):
@@ -19,22 +19,29 @@ class CatLegend(ListLegend):
 
         side = self._sizer[size]
 
+        options = dict(
+            frameon=False,
+            handleheight=1.0,
+            handlelength=1.0,
+            handletextpad=0.5,
+            labelspacing=0.3,
+            **kwargs,
+        )
+
         super().__init__(legend_items=legend_items,
-                         handleheight=side,
-                         handlelength=side,
-                         handletextpad=0.5,
-                         labelspacing=0.2,
-                         **kwargs)
+                         **options)
 
 
 class SizeLegend(ListLegend):
     """A special class use to create legend that represent size"""
 
-    def __init__(self, sizes, colors=None, labels=None, num=5, forceint=True, ascending=True, **kwargs):
+    def __init__(self, sizes, colors=None, labels=None, num=5, trim_min=True, forceint=True, ascending=True, **kwargs):
 
         smin, smax = np.nanmin(sizes), np.nanmax(sizes)
         dtype = int if forceint else float
         handles_sizes = np.linspace(smin, smax, num=num, dtype=dtype)
+        if trim_min:
+            handles_sizes = handles_sizes[1::]
         if colors is None:
             colors = ['black' for _ in range(num)]
         if labels is None:
@@ -55,4 +62,16 @@ class SizeLegend(ListLegend):
             size_handles = size_handles[::-1]
             size_labels = size_labels[::-1]
 
-        super().__init__(handles=size_handles, labels=size_labels, **kwargs)
+        options = dict(
+            frameon=False,
+            handleheight=1.0,
+            handlelength=1.0,
+            handletextpad=0.5,
+            labelspacing=0.5,
+            **kwargs
+        )
+
+        super().__init__(
+            handles=size_handles,
+            labels=size_labels,
+            **options)
